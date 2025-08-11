8e6660b711547b3e9dbb593bcc02cc916dab1bd0
EvgeniyaNesterkova <miledieva08@gmail.com>
2025-08-11 16:24:13 +0300

    add LastLoginAt  time.Time

--- internal/models/user.go
+++ internal/models/user.go
@@ -19,4 +19,5 @@ type User struct {
 	Email        string    `json:"email" db:"email"`
 	CreatedAt    time.Time `json:"created_at" db:"created_at"`
 	UpdatedAt    time.Time `json:"updated_at" db:"updated_at"`
+	LastLoginAt  time.Time `json:"last_login_at" db:"last_login_at"`
 }
