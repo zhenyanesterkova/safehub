a4faa97fceb3d02d016fc56cee10956213af1c62
EvgeniyaNesterkova <miledieva08@gmail.com>
2025-08-12 15:42:25 +0300

    fix migrations + add GetByUsername + fix tests

--- migrations/001_create_users.up.sql
+++ migrations/001_create_users.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    username VARCHAR(255) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
     salt VARCHAR(255),
